b7de90bd05032876434d1840a90b23f4c36bc1de
KatieAJenkins <kind2karma@gmail.com>
2017-06-02 10:16:42 -0600

    finished encode function

--- watneyDecoder.js
+++ watneyDecoder.js
@@ -1,9 +1,23 @@
-function decode(str){
+'use strict';
 
+let str = "Hello";
+let results;
+encode(str);
+decode(results)
+
+function decode(str){
+  console.log('results decode ', results);
 }
 
 function encode(str){
- 
+  var strArray = str.split('').map(function(element){
+    // console.log(element.charCodeAt(0).toString(16));
+        return element.charCodeAt(0).toString(16);
+  });
+  // console.log(strArray.join(""));
+  results = strArray.join("");
+  console.log(results);
+  return results;
 }
 
 function encodeArc(str) {
